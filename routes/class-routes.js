0617a381d5d94bfc14132562296bd09c5cbaac42
Gribni4ok <mr.vlads1998@gmail.com>
2022-05-12 06:53:56 +0400

    v3

--- routes/class-routes.js
+++ routes/class-routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const {getClasses} = require("../classes/classes.js");
 
-router.get("/classes",function(request,response){
-    response.render("classes/classes.hbs");
+router.get("/classes",async function(request,response){
+    var data = await getClasses();
+    console.log(data);
+    response.render("classes/classes.hbs", {data: data});
 });
+
+router.get("/classes/class/class/:id", async function(request,response){
+    const id = request.params.id;
+});
+
 module.exports = router;
