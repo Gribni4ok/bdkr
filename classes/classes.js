8c6682a590d9a52d196dcd7a679aa2260c2cbbe0
Gribni4ok <mr.vlads1998@gmail.com>
2022-05-12 06:54:13 +0400

    v4

--- classes/classes.js
+++ classes/classes.js
@@ -2,10 +2,12 @@ const pool = require("../js/init.js");
 
 async function getClasses(){
    var data = await pool.execute(`
-        SELECT classID, classname,
+        SELECT classID, classname
+        FROM classes
     `)
     .catch(err=>{
         console.log(err);
     });
+    return data[0];
 };
 module.exports = {getClasses}
