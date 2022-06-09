5ff67912225240e4e8ae947c71f3fbdc5731f0c4
Gribni4ok <mr.vlads1998@gmail.com>
2022-06-09 15:26:35 +0400

    tokenwork

--- classes/logins.js
+++ classes/logins.js
@@ -1,6 +1,7 @@
 const pool = require("../js/init.js");
 const bcrypt = require("bcrypt");
-const {saltRounds} = require("../consts/values.js");
+const jwt = require("jsonwebtoken");
+const {saltRounds, key} = require("../consts/values.js");
 
 async function createAdmin(data){
     var result;
@@ -18,4 +19,133 @@ async function createAdmin(data){
     })
     return result;
 }
-module.exports = {createAdmin};
+
+
+async function findUser(req,res,next){
+    var user = {
+        login: req.body.login,
+        password: req.body.password
+    }
+    var found;
+    if(found = await searchAdmins(user))
+    {
+        req.user = user;
+        next();
+    }
+    else if(found = await searchTeachers(user))
+    {
+        req.user = user;
+        next();
+    }
+    else if(found = await searchStudents(user))
+    {
+        req.user = user;
+        next();
+    }
+    else{
+        res.sendStatus(400);
+    }
+}
+
+async function searchAdmins(user){
+    var data = await pool.execute(`
+    SELECT * 
+    FROM admins
+    WHERE adminlogin = "${user.login}" 
+    LIMIT 1
+    `)
+    .catch((err)=>{
+        console.log(err);
+    })
+    if(bcrypt.compare(user.password, data[0][0].adminpassword))
+    {
+        var temp = {
+            role:2,
+            login: data[0].adminlogin
+        }
+        return temp;
+    }
+    else return false;
+}
+async function searchTeachers(user){
+    var data = await pool.execute(`
+    SELECT * 
+    FROM teachers
+    WHERE teacherlogin = "${user.login}" 
+    LIMIT 1
+    `)
+    .catch((err)=>{
+        console.log(err);
+    })
+    if(bcrypt.compare(user.password, data[0][0].teacherpassword))
+    {
+        var temp = {
+            role:1,
+            login: data[0][0].teacherlogin
+        }
+        return temp;
+    }
+    else return false;
+}
+async function searchStudents(user){
+    var data = await pool.execute(`
+    SELECT * 
+    FROM students
+    WHERE studentlogin = "${user.login}" 
+    LIMIT 1
+    `)
+    .catch((err)=>{
+        console.log(err);
+    })
+    if(bcrypt.compare(user.password, data[0][0].studentpassword))
+    {
+        var temp = {
+            role:0,
+            login: data[0][0].studentlogin
+        }
+        return temp;
+    }
+    else return false;
+}
+
+async function genToken(user){
+    const token = jwt.sign({ role: user.role, login: user.login },key,{ expiresIn: "24h"});
+    return token;
+}
+
+async function verifyToken(req,res,next){
+    const token = req.headers["token"];
+    if(!token)
+    {
+        res.redirect("/authorization");
+    }
+    else
+    {
+        req.user = jwt.verifyToken(token,key);
+        next();
+    }
+}
+
+async function checkIfTeacher(req,res,next){
+    if(req.user.role >0) next();
+    else res.redirect("/articles");
+}
+
+async function checkIfAdmin(req,res,next){
+    if(req.user.role >1) next();
+    else res.redirect("/articles");
+}
+
+async function checkIfNoAdmins(req,res,next){
+    var admins = await pool.execute(`
+        SELECT *
+        FROM admins
+    `)
+    .catch((err)=>{
+        console.log(err);
+    });
+    if(admins[0][0].length == 0) next();
+    else res.redirect("/authorization");
+}
+
+module.exports = {createAdmin, findUser, genToken, verifyToken, checkIfAdmin, checkIfTeacher};
--- routes/login-routes.js
+++ routes/login-routes.js
@@ -1,16 +1,27 @@
 const express = require("express");
 const router = express.Router();
-const {createAdmin} = require("../classes/logins.js");
+const {createAdmin, findUser, genToken} = require("../classes/logins.js");
 var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 
 router.get("/registration", function(request,response){
-    response.render("login/registration.hbs");
+    const title ="Регистрация"
+    response.render("login/registration.hbs",{title:title});
 });
 
 router.post("/registration", jsonParser, async function(request,response){
-    if(!request.body || await createAdmin(request.body)) response.sendStatus(400);
+    if(!request.body || !await createAdmin(request.body)) response.sendStatus(400);
     else response.sendStatus(200);
+});
+
+router.get("/authorization", function(request,response){
+    const title ="Авторизация";
+    response.render("login/authorization.hbs",{title:title});
+});
+
+router.post("/authorization", jsonParser, findUser, async function(request,response){
+    const token = await genToken(request.user);
+    response.json({token: token});
 });
 
 module.exports = router;
